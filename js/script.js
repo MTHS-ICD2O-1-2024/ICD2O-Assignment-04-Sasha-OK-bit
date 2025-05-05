feca4231eb5ae9a8f8824f43687b1651bada4dd3
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-05-05 16:47:02 +0000

    Testing point

--- js/script.js
+++ js/script.js
@@ -3,3 +3,36 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function calculaterandompreferrence () {
+  const Cheese= document.getElementById('option-cheese').checked
+  const Onions= document.getElementById('option-onions').checked
+  const Bacon= document.getElementById('option-bacon').checked
+  const Olives= document.getElementById('option-olives').checked
+  const Pineapples= document.getElementById('option-pineapples').checked
+  const Pepperionis= document.getElementById('option-pepperionis').checked
+  const Ham = document.getElementById('option-ham').checked
+
+  if (Cheese && Pepperionis) {
+  document.getElementById('answer1').innerHTML =
+  "<p>...</p>" 
+  }
+    else if(lengthA === lengthB === lengthC) {
+    document.getElementById('answer1').innerHTML =
+      "<p>You calculated towards a Equilateral triangle type </p>" 
+  }
+    else if(lengthA === lengthB !== lengthC || lengthA !== lengthB === lengthC || lengthC === lengthA !== lengthB) {
+    document.getElementById('answer1').innerHTML =
+      "<p>You calculated towards a Isocenes triangle type</p>" 
+  }
+    else if (lengthA !== lengthB !== lengthC && lengthC !== lengthA) {
+    document.getElementById('answer1').innerHTML =
+      "<p>You calculated towards a Scalene triangle type </p>" 
+  }
+    else {
+    document.getElementById('answer1').innerHTML =
+      "<p>You calculation got an error</p>" 
+
+}
+
+  }
