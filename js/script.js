b54834e02ab0fc22249ce5fa73438436e11d85d8
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-05-05 17:12:26 +0000

    Bruhs

--- js/script.js
+++ js/script.js
@@ -5,27 +5,27 @@
 // This file contains the JS functions for index.html
 
 function calculaterandompreferrence () {
-  const Cheese= document.getElementById('option-cheese')
-  const Onions= document.getElementById('option-onions')
-  const Bacon= document.getElementById('option-bacon')
-  const Olives= document.getElementById('option-olives')
-  const Pineapples= document.getElementById('option-pineapples')
-  const Pepperionis= document.getElementById('option-pepperionis')
-  const Ham = document.getElementById('option-ham')
+  const Cheese= document.getElementById('option-cheese').checked
+  const Onions= document.getElementById('option-onions').checked
+  const Bacon= document.getElementById('option-bacon').checked
+  const Olives= document.getElementById('option-olives').checked
+  const Pineapples= document.getElementById('option-pineapples').checked
+  const Pepperionis= document.getElementById('option-pepperionis').checked
+  const Ham = document.getElementById('option-ham').checked
 
-  if (((Cheese) || (Pepperionis))  == true) {
+  if ((Cheese || Pepperionis)  == true) {
   document.getElementById('answer1').innerHTML =
   "<p>Your pizza would taste American</p>" 
   }
-    else if(((Ham) || (Pineapples))  == true) {
+    else if((Ham || Pineapples)  == true) {
     document.getElementById('answer1').innerHTML =
       "<p>You got Hawaii Style Pizza</p>" 
   }
-    else if(((Olives))  == true ) {
+    else if((Olives)  == true ) {
     document.getElementById('answer1').innerHTML =
       "<p>You consider of something european</p>" 
   }
-    else if (((Bacon) || (Onions))  == true) {
+    else if ((Bacon || Onions)  == true) {
     document.getElementById('answer1').innerHTML =
       "<p> You're a odd person in Canada, nice </p>" 
   }
